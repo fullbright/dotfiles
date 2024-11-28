fbcaec85ef1da2615f80e156d12bc5c893d1421f
Sergio Afanou <full3right@gmail.com>
2024-11-28 14:12:45 +0100

    add script to reconfigure dotfiles

---
+++ .mybin/configure_dotfiles.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+git clone --bare https://github.com/fullbright/dotfiles.git $HOME/.dotfiles_bare
+
+function dotfiles {
+   /usr/bin/git --git-dir=$HOME/.dotfiles_bare/ --work-tree=$HOME $@
+}
+
+mkdir -p .dotfiles-backup
+
+dotfiles checkout
+
+if [ $? = 0 ]; then
+  echo "Checked out config.";
+  else
+    echo "Backing up pre-existing dot files.";
+    dotfiles checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .dotfiles-backup/{}
+fi;
+
+dotfiles checkout
+dotfiles config status.showUntrackedFiles no
