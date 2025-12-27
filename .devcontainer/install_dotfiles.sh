342d5226984b2843907b86f8fa2e06c4b154f9ce
fullbright <774136+fullbright@users.noreply.github.com>
2025-12-27 21:47:24 +0100

    Configure VSCode settings in install_dotfiles.sh
    Add script to configure VSCode settings from dotfiles

--- .devcontainer/install_dotfiles.sh
+++ .devcontainer/install_dotfiles.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
+echo "Configure vscode settings"
+cp "~/vscode/.config/Code/User/settings.json" "~/Library/Application Support/Code/User/"
+
+# cp "~/vscode/.config/Code/User/keybindings.json" "~/Library/Application Support/Code/User/"
+
 echo "I am not configured yet. Add your installation steps here."
